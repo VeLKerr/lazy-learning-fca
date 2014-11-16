1a71ed0f5379a222c4104012202874d7e7f71bf8
artush.ghandilyan@gmail.com <artush.ghandilyan@gmail.com@5ce5fc38-b33a-d36b-26a0-6c6eadf44afe>
2014-11-16 19:43:02 +0000

    Simple classification. Who has more weight and he wins. {'positive_negative': 112, 'negative_positive': 0, 'negative_negative': 15, 'positive_positive': 60, 'contradictory': 0}

--- trunk/artush_ghandilyan/generator/generators.py
+++ trunk/artush_ghandilyan/generator/generators.py
@@ -1,40 +1,48 @@
 import pprint
 import sys
 
-index = sys.argv[1]
-
-q=open("train"+index+".csv","r")
+q = open("train.csv","r")
 train = [ a.strip().split(",") for a in q]
-plus = [a for a in train if a[-1]=="positive"]
-minus = [a for a in train if a[-1]=="negative"]
-
-#print t
+plus = [a for a in train if a[0] == "1"]
+minus = [a for a in train if a[0] == "0"]
 q.close()
-w=open("test"+index+".csv","r")
+
+w = open("test.csv","r")
 unknown = [a.strip().split(",") for a in w]
 w.close()
 
-
 cv_res = {
- "positive_positive": 0,
- "positive_negative": 0,
- "negative_positive": 0,
- "negative_negative": 0,
- "contradictory": 0,
+    "positive_positive": 0,
+    "positive_negative": 0,
+    "negative_positive": 0,
+    "negative_negative": 0,
+    "contradictory": 0,
 }
 
-#attrib_names = [ 'class','a1','a2','a3','a4','a5','a6' ]
 attrib_names = [
-'top-left-square',
-'top-middle-square',
-' top-right-square',
-'middle-left-square',
-'middle-middle-square',
-'middle-right-square',
-'bottom-left-square',
-'bottom-middle-square',
-'bottom-right-square',
-'class'
+    'OVERALL_DIAGNOSIS',
+    'F1',
+    'F2',
+    'F3',
+    'F4',
+    'F5',
+    'F6',
+    'F7',
+    'F8',
+    'F9',
+    'F10',
+    'F11',
+    'F12',
+    'F13',
+    'F14',
+    'F15',
+    'F16',
+    'F17',
+    'F18',
+    'F19',
+    'F20',
+    'F21',
+    'F22'
 ]
 
 
@@ -43,59 +51,58 @@ def make_intent(example):
     return set([i+':'+str(k) for i,k in zip(attrib_names,example)])
     
 def check_hypothesis(context_plus, context_minus, example):
-  #  print example
     eintent = make_intent(example)
-  #  print eintent
-    eintent.discard('class:positive')
-    eintent.discard('class:negative')
+    eintent.discard('OVERALL_DIAGNOSIS:1')
+    eintent.discard('OVERALL_DIAGNOSIS:0')
     labels = {}
     global cv_res
     for e in context_plus:
         ei = make_intent(e)
         candidate_intent = ei & eintent
-        closure = [ make_intent(i) for i in context_minus if make_intent(i).issuperset(candidate_intent)]
+        closure = [make_intent(i) for i in context_minus if make_intent(i).issuperset(candidate_intent)]
         closure_size = len([i for i in closure if len(i)])
-    #    print closure
-        #print closure_size * 1.0 / len(context_minus)
         res = reduce(lambda x,y: x&y if x&y else x|y, closure ,set())
-        for cs in ['positive','negative']:
-            if 'class:'+cs in res:
+        for cs in ['1', '0']:
+            if 'OVERALL_DIAGNOSIS:'+cs in res:
                 labels[cs] = True
                 labels[cs+'_res'] = candidate_intent
-                labels[cs+'_total_weight'] = labels.get(cs+'_total_weight',0) +closure_size * 1.0 / len(context_minus) / len(context_plus)
+                labels[cs+'_total_weight'] = \
+                    labels.get(cs+'_total_weight', 0) + closure_size * 1.0 / len(context_minus) / len(context_plus)
     for e in context_minus:
         ei = make_intent(e)
         candidate_intent = ei & eintent
-        closure = [ make_intent(i) for i in context_plus if make_intent(i).issuperset(candidate_intent)]
+        closure = [make_intent(i) for i in context_plus if make_intent(i).issuperset(candidate_intent)]
         closure_size = len([i for i in closure if len(i)])
-        #print closure_size * 1.0 / len(context_plus)
-        res = reduce(lambda x,y: x&y if x&y else x|y, closure, set())
-        for cs in ['positive','negative']:
-            if 'class:'+cs in res:
+        res = reduce(lambda x, y: x & y if x & y else x | y, closure, set())
+        for cs in ['1', '0']:
+            if 'OVERALL_DIAGNOSIS:'+cs in res:
                 labels[cs] = True
                 labels[cs+'_res'] = candidate_intent
-                labels[cs+'_total_weight'] = labels.get(cs+'_total_weight',0) +closure_size * 1.0 / len(context_plus) / len(context_minus)
+                labels[cs+'_total_weight'] = \
+                    labels.get(cs+'_total_weight', 0) + closure_size * 1.0 / len(context_plus) / len(context_minus)
     print eintent
     print labels
-    if labels.get("positive",False) and labels.get("negative",False):
+    if labels.get("1", False) and labels.get("0", False) and labels.get("1_total_weight", 0) == labels.get("0_total_weight", 0):
        cv_res["contradictory"] += 1
        return
-    if example[-1] == "positive" and labels.get("positive",False):
+    if example[0] == "1" and labels.get("1", False) and labels.get("1_total_weight", 0) > labels.get("0_total_weight", 0):
        cv_res["positive_positive"] += 1
-    if example[-1] == "negative" and labels.get("positive",False):
+    if example[0] == "0" and labels.get("1", False) and labels.get("1_total_weight", 0) > labels.get("0_total_weight", 0):
        cv_res["negative_positive"] += 1
-    if example[-1] == "positive" and labels.get("negative",False):
+    if example[0] == "1" and labels.get("0", False) and labels.get("1_total_weight", 0) < labels.get("0_total_weight", 0):
        cv_res["positive_negative"] += 1
-    if example[-1] == "negative" and labels.get("negative",False):
+    if example[0] == "0" and labels.get("0", False) and labels.get("1_total_weight", 0) < labels.get("0_total_weight", 0):
        cv_res["negative_negative"] += 1
+
+
+
 
 #sanity check:
 #check_hypothesis(plus_examples, minus_examples, plus_examples[3])
 i = 0
 for elem in unknown:
-    #print elem
     print "done"
     i += 1
     check_hypothesis(plus, minus, elem)
-#    if i == 3: break
+
 print cv_res
