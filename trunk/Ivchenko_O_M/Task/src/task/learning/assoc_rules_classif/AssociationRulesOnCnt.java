d9aaebc9573ce3c8169a40a4309070ea094445d2
k.velkerr@gmail.com <k.velkerr@gmail.com@5ce5fc38-b33a-d36b-26a0-6c6eadf44afe>
2014-12-02 09:12:41 +0000

    Fixed many different small bugs and improved results.

--- trunk/Ivchenko_O_M/Task/src/task/learning/assoc_rules_classif/AssociationRulesOnCnt.java
+++ trunk/Ivchenko_O_M/Task/src/task/learning/assoc_rules_classif/AssociationRulesOnCnt.java
@@ -8,45 +8,27 @@ import task.learning.Classificator;
  *
  * @author Ivchenko Oleg (Kirius VeLKerr)
  */
-public class AssociationRulesOnCnt implements AssociationRules{
-    private int power;
-    private double confidence;
-    private double support;
-    private int cnt;
+public class AssociationRulesOnCnt extends AssociationRules{
+    private final double lim;
 
-    public AssociationRulesOnCnt() {
+    public AssociationRulesOnCnt(double lim) {
         this.power = 0;
         this.confidence = 0.0;
         this.support = 0.0;
         this.cnt = 0;
+        this.lim = lim;
     }
     
     @Override
     public void takeIntoAccount(Classificator cl, Acceptability ac){
         double conf = cl.getConfidence(ac);
-        double supp = cl.getSupport(ac);
-        if(conf != 0.0 && supp > 2){
-            power += cl.getPower();
-            support += supp;
-            confidence += conf;
-            cnt++;
-        }
-    }
-    
-    @Override
-    public double getCoef(int coefImportanceCnt){
-        switch(coefImportanceCnt){
-            case 0:{
-                return cnt;
-            }
-            case 1:{
-                return confidence;
-            }
-            case 2:{
-                return support;
-            }
-            default:{
-                return power;
+        if(conf >= lim){
+            double supp = cl.getSupport(ac);
+            if(supp > 2){
+                power += cl.getPower();
+                support += supp;
+                confidence += conf;
+                cnt++;
             }
         }
     }
