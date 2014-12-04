cbf72f87c71464cadb7617c8b9cf32624810ddb5
denis.kondrushkin <denis.kondrushkin@5ce5fc38-b33a-d36b-26a0-6c6eadf44afe>
2014-12-04 23:46:10 +0000

    [branch] Cross-validation support release.

--- trunk/kondrushkin_denis/code/CPlusPlusVersion/CPlusPlusVersion/loader.h
+++ trunk/kondrushkin_denis/code/CPlusPlusVersion/CPlusPlusVersion/loader.h
@@ -1,36 +1,73 @@
 #pragma once
+
+
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include "defines.h"
 
 
 class Loader {
+
+protected:
+	const char* _path;
+
 public:
+	Loader(){};
+	Loader(const char* path) : _path(path){}
+
+public:
+	virtual void load_context(
+		const int num_attrs,
+		std::vector<const char*> &positive_context,
+		std::vector<const char*> &negative_context) const = 0;
+
 	virtual void load_context(
 		const char* path,
 		const int num_attrs,
 		std::vector<const char*> &positive_context,
 		std::vector<const char*> &negative_context) const = 0;
 
-	virtual void load_test_data(
+	virtual void load_data(
+		const int num_attrs,
+		std::vector<const char*> &test_data,
+		std::vector<const char*> &_answer) const = 0;
+	
+	virtual void load_data(
 		const char* path,
 		const int num_attrs,
-		std::vector<const char*> &test_data) const = 0;
+		std::vector<const char*> &test_data,
+		std::vector<const char*> &_answer) const = 0;
 
 };
 
 
-class LoaderA : public Loader {
+class SLoader : public Loader {
 
 public:
+	SLoader(){};
+	SLoader(const char* path) : Loader(path){}
+
+public:
+	void load_context(
+		const int num_attrs,
+		std::vector<const char*> &positive_context,
+		std::vector<const char*> &negative_context) const;
+
 	void load_context(
 		const char* path,
 		const int num_attrs,
 		std::vector<const char*> &positive_context,
 		std::vector<const char*> &negative_context) const;
 
-	void load_test_data(
+	void load_data(
+		const int num_attrs,
+		std::vector<const char*> &test_data,
+		std::vector<const char*> &answer) const;
+	
+	void load_data(
 		const char* path,
 		const int num_attrs,
-		std::vector<const char*> &test_data) const;
+		std::vector<const char*> &test_data,
+		std::vector<const char*> &answer) const;
 };
