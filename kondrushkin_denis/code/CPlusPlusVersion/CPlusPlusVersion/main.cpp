ea62ae49a138ae97a26e18f42fc68b1907ff2d7f
denis.kondrushkin <denis.kondrushkin@5ce5fc38-b33a-d36b-26a0-6c6eadf44afe>
2014-12-16 20:49:58 +0000

    [branch] HypothesisTestingWeightedAlgorithm 1.0 release (avg rate 0.96).

--- kondrushkin_denis/code/CPlusPlusVersion/CPlusPlusVersion/main.cpp
+++ kondrushkin_denis/code/CPlusPlusVersion/CPlusPlusVersion/main.cpp
@@ -12,27 +12,32 @@
 
 int main(){
 
-	const char* path = "D:\\COURSES\\HSE\\Ordered Sets\\HW\\lazy-learning-fca\\kondrushkin_denis\\data\\tic-tac-toe.data.txt"; 
 	const char* path_res = "D:\\COURSES\\HSE\\Ordered Sets\\HW\\lazy-learning-fca\\kondrushkin_denis\\data\\results.txt"; 
+
+	const char* path = "D:\\COURSES\\HSE\\Ordered Sets\\HW\\lazy-learning-fca\\kondrushkin_denis\\data\\tic-tac-toe.data.txt"; 
 	const int num_attrs = 9;
+
+	//const char* path = "D:\\COURSES\\HSE\\Ordered Sets\\HW\\lazy-learning-fca\\kondrushkin_denis\\data\\connect-4.data"; 
+	//const int num_attrs = 42;
+
 	const int k = 30;
 
 	// loads data for validation
 	Data input_data = Data(num_attrs, SLoader(path));	
 
 	// set algorithms for validation
-	std::vector<Algorithm*> algoritms = std::vector<Algorithm*>();
-	algoritms.push_back(new StupidLazyAlgorithm());
-	algoritms.push_back(new HammingDistanceLazyAlgorithm());
-	algoritms.push_back(new HammDistWeightedAlgorithm());
-	algoritms.push_back(new HypothesisTestingAlgorithm());
+	std::vector<Algorithm*> algorithms = std::vector<Algorithm*>();
+	algorithms.push_back(new HammingDistanceLazyAlgorithm());
+	algorithms.push_back(new HammDistWeightedAlgorithm());
+	algorithms.push_back(new HypothesisTestingAlgorithm());
+	algorithms.push_back(new HypothesisTestingWeightedAlgorithm());
 	
 	// validates
 	CrossValidator cross_validator = CrossValidator(input_data, k);
-	cross_validator.validate(path_res, algoritms);
+	cross_validator.validate(path_res, algorithms);
 
 	// frees memory
-	for each (Algorithm* alg in algoritms){
+	for each (Algorithm* alg in algorithms){
 		delete alg;
 	}
 
