2d3c4b66dc7c7e2077b52d5030a45fd1071381ba
netutmaksim <netutmaksim@gmail.com>
2014-12-26 12:53:25 +0000

    adapted generator classifier; change of calculation of estimators

--- baytel_maxim/test_generators.py
+++ baytel_maxim/test_generators.py
@@ -1,19 +1,5 @@
+﻿
 import pprint
-import sys
-
-index = sys.argv[1]
-
-q=open("train"+index+".csv","r")
-train = [ a.strip().split(",") for a in q]
-plus = [a for a in train if a[-1]=="positive"]
-minus = [a for a in train if a[-1]=="negative"]
-
-#print t
-q.close()
-w=open("test"+index+".csv","r")
-unknown = [a.strip().split(",") for a in w]
-w.close()
-
 
 cv_res = {
  "positive_positive": 0,
@@ -41,61 +27,142 @@ attrib_names = [
 def make_intent(example):
     global attrib_names
     return set([i+':'+str(k) for i,k in zip(attrib_names,example)])
+
+
+def classification(context_plus, context_minus, unknown,alpha):
+
+    global cv_res
+    cv_res["positive_positive"] = 0
+    cv_res["positive_negative"] = 0
+    cv_res["negative_positive"] = 0
+    cv_res["negative_negative"] = 0
+    cv_res["contradictory"] = 0
+    cv_res["not_classified"] = 0
+    for elem in unknown:
+        check_hypothesis(context_plus, context_minus, elem,alpha)
+    return cv_res
+
     
-def check_hypothesis(context_plus, context_minus, example):
+def check_hypothesis(context_plus, context_minus,example,alpha):
   #  print example
     eintent = make_intent(example)
   #  print eintent
     eintent.discard('class:positive')
     eintent.discard('class:negative')
-    labels = {}
+
+    posVotes=0
+    negVotes=0
+
     global cv_res
     for e in context_plus:
         ei = make_intent(e)
-        candidate_intent = ei & eintent
-        closure = [ make_intent(i) for i in context_minus if make_intent(i).issuperset(candidate_intent)]
-        closure_size = len([i for i in closure if len(i)])
-    #    print closure
-        #print closure_size * 1.0 / len(context_minus)
-        res = reduce(lambda x,y: x&y if x&y else x|y, closure ,set())
-        for cs in ['positive','negative']:
-            if 'class:'+cs in res:
-                labels[cs] = True
-                labels[cs+'_res'] = candidate_intent
-                labels[cs+'_total_weight'] = labels.get(cs+'_total_weight',0) +closure_size * 1.0 / len(context_minus) / len(context_plus)
+        candidate_intent = ei & eintent #перечечение ? с объектом положительного контекста
+
+        #пересечение мало - не будем ничего с ним делать...
+        if len(candidate_intent) < alpha * len(eintent):
+            continue
+
+        countPosObjs=0
+
+        for key in context_plus:
+            if len(make_intent(key) & candidate_intent) == len(candidate_intent):
+                countPosObjs+=1
+
+        countNegObjs=0
+        for key in context_minus:
+            if len(make_intent(key) & candidate_intent) == len(candidate_intent):
+                countNegObjs+=1
+
+        if countPosObjs > countNegObjs:
+            posVotes += 1
+
     for e in context_minus:
         ei = make_intent(e)
-        candidate_intent = ei & eintent
-        closure = [ make_intent(i) for i in context_plus if make_intent(i).issuperset(candidate_intent)]
-        closure_size = len([i for i in closure if len(i)])
-        #print closure_size * 1.0 / len(context_plus)
-        res = reduce(lambda x,y: x&y if x&y else x|y, closure, set())
-        for cs in ['positive','negative']:
-            if 'class:'+cs in res:
-                labels[cs] = True
-                labels[cs+'_res'] = candidate_intent
-                labels[cs+'_total_weight'] = labels.get(cs+'_total_weight',0) +closure_size * 1.0 / len(context_plus) / len(context_minus)
-    print eintent
-    print labels
-    if labels.get("positive",False) and labels.get("negative",False):
-       cv_res["contradictory"] += 1
-       return
-    if example[-1] == "positive" and labels.get("positive",False):
-       cv_res["positive_positive"] += 1
-    if example[-1] == "negative" and labels.get("positive",False):
-       cv_res["negative_positive"] += 1
-    if example[-1] == "positive" and labels.get("negative",False):
-       cv_res["positive_negative"] += 1
-    if example[-1] == "negative" and labels.get("negative",False):
-       cv_res["negative_negative"] += 1
+        candidate_intent = ei & eintent #перечечение ? с объектом положительного контекста
 
-#sanity check:
-#check_hypothesis(plus_examples, minus_examples, plus_examples[3])
-i = 0
-for elem in unknown:
-    #print elem
-    print "done"
-    i += 1
-    check_hypothesis(plus, minus, elem)
-#    if i == 3: break
-print cv_res
+        #пересечение мало - не будем ничего с ним делать...
+        if len(candidate_intent) < alpha * len(eintent):
+            continue
+
+        countPosObjs=0
+
+        for key in context_plus:
+            if len(make_intent(key) & candidate_intent) == len(candidate_intent):
+                countPosObjs+=1
+
+        countNegObjs=0
+        for key in context_minus:
+            if len(make_intent(key) & candidate_intent) == len(candidate_intent):
+                countNegObjs+=1
+
+        if countNegObjs > countPosObjs:
+            negVotes += 1
+
+    if posVotes == negVotes:
+        if posVotes > 0:
+            cv_res["contradictory"] += 1
+        else:
+            cv_res["not_classified"] += 1
+    else:
+        if negVotes < posVotes:
+            predicted_class = "positive"
+            if predicted_class == example[-1]:
+                cv_res["positive_positive"] += 1
+            else:
+                cv_res["negative_positive"] += 1
+        else:
+            predicted_class = "negative"
+            if predicted_class == example[-1]:
+                cv_res["negative_negative"] += 1
+            else:
+                cv_res["positive_negative"] += 1
+
+def statistics(results):
+    count = len(results)
+    res={}
+
+    res['Sensitivity (TPR) of the procedure is ']           = 0
+    res['Fall-out (FPR) of the procedure is ']              = 0
+    res['Precision (PPV) of the procedure is ']             = 0
+    res['False omission rate (FOR) of the procedure is ']   = 0
+    res['Accuracy of the procedure is ']                    = 0
+
+    for element in results:
+
+        res['Sensitivity (TPR) of the procedure is ']           += element['positive_positive']/(element['positive_positive'] + element['positive_negative'])
+        res['Fall-out (FPR) of the procedure is ']              += element['negative_positive']/(element['negative_negative'] + element['negative_positive'])
+        res['Precision (PPV) of the procedure is ']             += element['positive_positive']/(element['positive_positive'] + element['negative_positive'])
+        res['False omission rate (FOR) of the procedure is ']   += element['positive_negative']/(element['positive_negative'] + element['negative_negative'])
+        res['Accuracy of the procedure is ']                    += (element['positive_positive'] + element['negative_negative'])/(element['positive_positive'] + element['positive_negative'] + element['negative_positive'] + element['negative_negative'])
+
+    resPercentage={}
+
+    for key in res:
+        resPercentage[key] = str(round(res[key]*100/count,2))+'%'
+
+    return resPercentage
+
+results = []
+
+alpha=0.7
+
+for index in range(1,11):
+    q=open("data/train"+str(index)+".csv","r")
+    train = [ a.strip().split(",") for a in q]
+    plus = [a for a in train if a[-1]=="positive"]
+    minus = [a for a in train if a[-1]=="negative"]
+    q.close()
+
+    w=open("data/test"+str(index)+".csv","r")
+    unknown = [a.strip().split(",") for a in w]
+
+    w.close()
+    unknown.pop(0)
+    results.append(classification(plus, minus, unknown,alpha))
+
+
+pprint.pprint(results)
+
+stats = statistics(results)
+
+pprint.pprint(stats)
